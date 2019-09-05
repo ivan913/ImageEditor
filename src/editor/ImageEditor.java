31d356b16dbe89f28ca866dcd5e793a66a861450
ivan913 <vanya.golochtchekov@gmail.com>
2019-09-05 13:11:42 -0600

    commit 3

--- src/editor/ImageEditor.java
+++ src/editor/ImageEditor.java
@@ -1,8 +1,51 @@
 package editor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ImageEditor {
 
+
     public static void main(String args[]){
+        File input = new File(args[0]);
+        File output = new File(args[1]);
+        String command = args[2];
+
+        Pixel[][] image = readFile(input);
 
     }
+
+    static Pixel[][] readFile(File input){
+        try{
+            Scanner scanner = new Scanner(input);
+            scanner.next(); //expect P3
+
+            int width, height;
+
+            width = scanner.nextInt();
+            height = scanner.nextInt();
+
+            scanner.nextInt(); //expect 255
+
+            Pixel[][] image = new Pixel[width][height];
+
+            for(int row = 0; row < width; row += 1){
+                for(int column = 0; column < height; column += 1){
+                    int red = scanner.nextInt();
+                    int green = scanner.nextInt();
+                    int blue = scanner.nextInt();
+                    image[row][column] = new Pixel(red, green, blue);
+                }
+            }
+            return image;
+        }catch(FileNotFoundException e){
+            System.out.println("File not Found");
+            return null;
+        }
+    }
+
+
+
 }
---
+++ src/editor/Pixel.java
@@ -0,0 +1,39 @@
+package editor;
+
+public class Pixel {
+    int red, green, blue;
+
+    public Pixel(int red, int green, int blue){
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public Pixel Invert(){
+        
+    }
+}
